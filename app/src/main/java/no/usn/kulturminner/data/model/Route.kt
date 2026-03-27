830f8263ebfb615aa6b7c03b6185970aa4d95f34
Kaj Kleiva <kaj.kleiva@hotmail.com>
2026-03-28 00:16:26 +0100

    Oppdatering av dataklasser (punkt og ruter)

--- app/src/main/java/no/usn/kulturminner/data/model/Route.kt
+++ app/src/main/java/no/usn/kulturminner/data/model/Route.kt
@@ -1,10 +1,12 @@
 package no.usn.kulturminner.data.model
 
+// Hovedklasse for rute
 data class Route(
     val id: String,
     val name: String,
     val color: String? = null,
-    val coordinates: List<LatLng>
+    val coordinates: List<LatLng>                   // Liste med koordinatpunkt
 )
 
-data class LatLng(val lat: Double, val lng: Double) // enkel hjelpeklasse
+// Enkel hjelpeklasse
+data class LatLng(val lat: Double, val lng: Double)
