5609db55bb06be16da843a6b2df06f008739427e
Kaj Kleiva <kaj.kleiva@hotmail.com>
2026-04-21 03:30:35 +0200

    SerialName SectionDto.kt

--- app/src/main/java/no/usn/kulturminner/data/api/SectionDto.kt
+++ app/src/main/java/no/usn/kulturminner/data/api/SectionDto.kt
@@ -1,12 +1,14 @@
 package no.usn.kulturminner.data.api
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SectionDto(
     val id: String? = null,
+    @SerialName("point_id") val pointId: String? = null,
     val heading: String? = null,
     val text: String? = null,
-    val imageUrl: String? = null,
-    val videoUrl: String? = null
+    @SerialName("image_url") val imageUrl: String? = null,
+    @SerialName("video_url") val videoUrl: String? = null
 )
